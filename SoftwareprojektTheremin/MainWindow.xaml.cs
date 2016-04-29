b80b5424b046cf1fb7bd70359a5b1abbdb2cf3b4
tvoelkel <tobias.voelkel96@gmail.com>
2016-04-29 14:49:19 +0200

    Projekt erstellt

---
+++ SoftwareprojektTheremin/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoftwareprojektTheremin
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PXCMSession session;
+        private PXCMSenseManager senseManager;
+        private Thread update;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Configure RealSense session and SenseManager interface
+            session = PXCMSession.CreateInstance();
+            senseManager = session.CreateSenseManager();
+            senseManager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 640, 480, 30);
+            senseManager.Init();
+
+            // Start Update thread
+            update = new Thread(new ThreadStart(Update));
+            update.Start();
+        }
+
+        private void Update()
+        {
+            // Start AcquireFrame-ReleaseFrame loop
+            while (senseManager.AcquireFrame(true) >= pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                PXCMCapture.Sample sample = senseManager.QuerySample();
+                Bitmap colorBitmap;
+                PXCMImage.ImageData colorData;
+
+                // Get color image data
+                sample.color.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32, out colorData);
+                colorBitmap = colorData.ToBitmap(0, sample.color.info.width, sample.color.info.height);
+
+                // Update UI
+                Render(colorBitmap);
+
+                // Release frame
+                colorBitmap.Dispose();
+                sample.color.ReleaseAccess(colorData);
+                senseManager.ReleaseFrame();
+            }
+        }
+
+        private void Render(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage = ConvertBitmap(bitmap);
+
+            if (bitmapImage != null)
+            {
+                // Update the WPF Image control
+                this.Dispatcher.Invoke(System.Windows.Threading.DispatcherPriority.Normal, new Action(delegate ()
+                {
+                    imgStream.Source = bitmapImage;
+                }));
+            }
+        }
+
+        private BitmapImage ConvertBitmap(Bitmap bitmap)
+        {
+            BitmapImage bitmapImage = null;
+
+            if (bitmap != null)
+            {
+                MemoryStream memoryStream = new MemoryStream();
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+                bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+            }
+
+            return bitmapImage;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ShutDown();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            ShutDown();
+            this.Close();
+        }
+
+        private void ShutDown()
+        {
+            // Stop the Update thread
+            update.Abort();
+
+            // Dispose RealSense objects
+            senseManager.Dispose();
+            session.Dispose();
+        }
+    }
+}
